61778c2db7deef41cdebe19dc2db1cd2967513dd
Boris Besemer <borisbesemer@gmail.com>
2025-09-08 12:46:42 +0200

    fix: show increased size on tool code

--- components/code-block.tsx
+++ components/code-block.tsx
@@ -1,28 +1,28 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { highlight as sh } from "sugar-high"
-import { cn } from "@/lib/utils"
-import { ScrollArea } from "@/components/ui/scroll-area"
+import * as React from "react";
+import { highlight as sh } from "sugar-high";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { cn } from "@/lib/utils";
 
 type CodeBlockProps = {
-  code: string
-  className?: string
-}
+	code: string;
+	className?: string;
+};
 
 export function CodeBlock({ code, className }: CodeBlockProps) {
-  const html = React.useMemo(() => sh(code), [code])
+	const html = React.useMemo(() => sh(code), [code]);
 
-  return (
-    <ScrollArea className="flex flex-col overflow-hidden max-h-96">
-      <pre className={cn("py-2", className)}>
-        <code
-          dangerouslySetInnerHTML={{ __html: html }}
-          className="font-geist-mono text-[13px] leading-2.5"
-        />
-      </pre>
-    </ScrollArea>
-  )
+	return (
+		<ScrollArea className="flex flex-col overflow-hidden max-h-126">
+			<pre className={cn("py-2", className)}>
+				<code
+					dangerouslySetInnerHTML={{ __html: html }}
+					className="font-geist-mono text-[13px] leading-1"
+				/>
+			</pre>
+		</ScrollArea>
+	);
 }
 
-export default CodeBlock
+export default CodeBlock;
